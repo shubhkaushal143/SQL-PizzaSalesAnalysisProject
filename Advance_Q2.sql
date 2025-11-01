83df2300073fa57aadc4a8e5817d5f00de595408
Shubham Chandra Kaushal <79314983+shubhkaushal143@users.noreply.github.com>
2025-11-01 19:05:44 +0530

    sql-analysis-query

---
+++ Advance_Q2.sql
@@ -0,0 +1,17 @@
+-- Analyze the cumulative revenue generated over time.
+
+
+select order_date,
+sum(revenue) over (order by order_date) as cum_revnue
+from
+(SELECT 
+    orders.order_date,
+    SUM(order_details.quantity * pizzas.price) AS revenue
+FROM
+    order_details
+        JOIN
+    pizzas ON order_details.pizza_id = pizzas.pizza_id
+        JOIN
+    orders ON orders.order_id = order_details.order_id
+GROUP BY orders.order_date) as sales
+;
---
+++ IQ_4.sql
@@ -0,0 +1,13 @@
+-- Group the orders by date and 
+-- calculate the average number of pizzas ordered per day.
+
+
+SELECT 
+    ROUND(AVG(quantity), 0) AS avg_number_of_pizzas
+FROM
+    (SELECT 
+        orders.order_date, SUM(order_details.quantity) AS quantity
+    FROM
+        orders
+    JOIN order_details ON orders.order_id = order_details.order_id
+    GROUP BY orders.order_date) AS order_quantity;
